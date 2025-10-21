72686574ab23918b2dfc850071068d9918e23d37
vishnuSharma04 <155941721+vishnuSharma04@users.noreply.github.com>
2025-10-21 22:12:05 +0530

    Updated cal.c

--- cal.c
+++ cal.c
@@ -2,74 +2,107 @@
 #include <ctype.h>
 #include <string.h>
 
-const char* p; 
-int err;       
+typedef enum {
+    ERRNONE = 0,
+    ERRDIVZERO,
+    ERRINVALID
+} ErrorCode;
 
-void skip() {
-while (*p && isspace(*p)) p++;
+static const char* ptr;
+static ErrorCode err;
+
+static void skipSpace(void);
+static int readNumber(void);
+static int readPrimary(void);
+static int readTerm(void);
+static int readExpr(void);
+
+static void skipSpace(void) {
+    while (*ptr && isspace((unsigned char)*ptr)) ptr++;
 }
 
-int num() {
-    skip();
-    if (!isdigit(*p)) {
-err = 2; return 0;
-}
-    int v = 0;
-    while (isdigit(*p)) {
-      v = v * 10 + (*p - '0'); p++; 
+static int readNumber(void) {
+    skipSpace();
+    if (!isdigit((unsigned char)*ptr)) {
+        err = ERRINVALID;
+        return 0;
     }
-    return v;
+    int val = 0;
+    while (isdigit((unsigned char)*ptr)) {
+        val = val * 10 + (*ptr - '0');
+        ptr++;
+    }
+    return val;
 }
 
-int fact() {
-  return num(); 
-}
-
-int term() {
-    int v = fact();
-    while (!err) {
-        skip();
-        if (*p == '*') { p++; v *= fact(); }
-        else if (*p == '/') { 
-            p++; 
-            int n = fact();
-            if (n == 0) {
-              err = 1;
-              return 0; 
+static int readPrimary(void) {
+    skipSpace();
+    if (*ptr == '(') {
+        ptr++;
+        int v = readExpr();
+        skipSpace();
+        if (*ptr == ')') {
+            ptr++;
+            return v;
+        } else {
+            err = ERRINVALID;
+            return 0;
         }
-            v /= n; 
+    }
+    if (*ptr == '+' || *ptr == '-') {
+        char sign = *ptr++;
+        int v = readPrimary();
+        return (sign == '-') ? -v : v;
+    }
+    return readNumber();
+}
+
+static int readTerm(void) {
+    int v = readPrimary();
+    while (err == ERRNONE) {
+        skipSpace();
+        if (*ptr == '*') {
+            ptr++;
+            v *= readPrimary();
+        } else if (*ptr == '/') {
+            ptr++;
+            int n = readPrimary();
+            if (n == 0) {
+                err = ERRDIVZERO;
+                return 0;
+            }
+            v /= n;
         } else break;
     }
     return v;
 }
 
-int expr() {
-    int v = term();
-    while (!err) {
-        skip();
-        if (*p == '+') {
-          p++; v += term();
-        }
-        else if (*p == '-') {
-          p++; v -= term();
-        }
-        else break;
+static int readExpr(void) {
+    int v = readTerm();
+    while (err == ERRNONE) {
+        skipSpace();
+        if (*ptr == '+') {
+            ptr++;
+            v += readTerm();
+        } else if (*ptr == '-') {
+            ptr++;
+            v -= readTerm();
+        } else break;
     }
     return v;
 }
 
-int main() {
+int main(void) {
     char buf[256];
     if (!fgets(buf, sizeof(buf), stdin)) return 0;
-    buf[strcspn(buf, "\n")] = 0;
-
-    p = buf; err = 0;
-    int v = expr();
-    skip();
-    if (*p) err = 2;
-
-    if (err == 1) printf("Error: Division by zero.\n");
-    else if (err == 2 || buf[0] == '\0') printf("Error: Invalid expression.\n");
+    buf[strcspn(buf, "\n")] = '\0';
+    ptr = buf;
+    err = ERRNONE;
+    int v = readExpr();
+    skipSpace();
+    if (*ptr != '\0' || buf[0] == '\0') err = ERRINVALID;
+    if (err == ERRDIVZERO) printf("Error: Division by zero.\n");
+    else if (err == ERRINVALID) printf("Error: Invalid expression.\n");
     else printf("%d\n", v);
     return 0;
 }
